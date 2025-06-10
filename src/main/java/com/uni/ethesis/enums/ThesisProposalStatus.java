80a8eaea623dc0ceda1d60ca739995acc757f507
Daniel Denisov <danikmass121@gmail.com>
2025-06-10 22:57:47 +0300

    add ThesisProposalStatus and ThesisStatus enums for thesis workflow management

---
+++ src/main/java/com/uni/ethesis/enums/ThesisProposalStatus.java
@@ -0,0 +1,9 @@
+package com.uni.ethesis.enums;
+
+public enum ThesisProposalStatus
+{
+    PENDING,
+    APPROVED,
+    REJECTED,
+    DEFENDED
+}
---
+++ src/main/java/com/uni/ethesis/enums/ThesisStatus.java
@@ -0,0 +1,8 @@
+package com.uni.ethesis.enums;
+
+public enum ThesisStatus {
+    WAITING_FOR_REVIEW,
+    WAITING_FOR_DEFENSE,
+    DEFENDED,
+    FAILED;
+}
